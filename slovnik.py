b789c5e105f0bb249374122dae2c70a0b7cfd12d
rbenesova <renata.benesova@infis.cz>
2025-05-13 09:32:00 +0200

    Add files via upload

---
+++ slovnik.py
@@ -0,0 +1,32 @@
+znamky = {
+    "Anna":[4,2,1,5],
+}
+
+# ukol4
+predmety ={
+    "matika":["anna", "jan"],
+    "ajina":["katka","adam"],
+    "fyzika":["elena"],
+}
+# ukol5
+filmy = {
+    "Inception":{
+        "režiser":"Christopher Nolan",
+        "rok":2010,
+    },
+    "Shrek":{
+        "režiser":"Andrew Adamson",
+        "rok" :2001  
+        }
+}
+# ukol6
+hrac= {
+    "jmeno":"adam",
+    "uroven":5,
+    "inventar":["meč","lektvar","štít"],
+
+    }
+# ukol7
+hrac["inventar"].append("klic")
+
+# ukol8
